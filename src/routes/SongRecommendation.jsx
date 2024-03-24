0d63b80f243d256291c63db4e0d7610d475b02a3
Ulises <ulisescappa@gmail.com>
2024-03-23 23:58:32 -0300

    Add react-player. Add basic form with a basic recommmendation list. The user could input a name, description and a youtube link, when it's submited the video is showed in the recommendtion list section

--- src/routes/SongRecommendation.jsx
+++ src/routes/SongRecommendation.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactPlayer from 'react-player'
 
 const SongRecomendation = () => {
+  //for onChange event
+  const [youtubeVideo, setYoutubeVideo] = useState('');
+  // for submit events
+  const [youtubeURL, setYoutubeURL] = useState('https://www.youtube.com/watch?v=jbiw2kKXh1g&ab_channel=InMundos-Topic');
+
+  const handleYoutubeChange = (e) => {
+    setYoutubeVideo(e.target.value)
+  }
+
+  const handleYoutubeSubmit=(e)=>{
+    e.preventDefault();
+    setYoutubeURL(youtubeVideo)
+
+  }
+
   return (
-    <div>SongRecomendation</div>
+    <>
+          <div className="flex">
+          <form className="w-1/2 p-4"
+          onSubmit={handleYoutubeSubmit}>
+            <div className="mb-4">
+              <input type="text" 
+              placeholder="Name" 
+              className="w-full border border-gray-300 rounded-md p-2"
+               />
+            </div>
+            <div className="mb-4">
+              <textarea placeholder="Description" className="w-full border border-gray-300 rounded-md p-2"></textarea>
+            </div>
+            <div className="mb-4">
+              <input type="text" placeholder="Link" required 
+              className="w-full border border-gray-300 rounded-md p-2"
+              onChange={handleYoutubeChange} />
+            </div>
+            <div>
+              <button type='submit' className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Submit</button>
+            </div>
+          </form>
+          <div className="w-1/2 p-4">
+            <div className="bg-gray-200 h-full p-4 rounded-md">
+              <h2 className="text-lg font-semibold mb-2">Recommendation List</h2>
+              <div className="youtube-box">
+                <ReactPlayer url={youtubeURL}
+                controls />
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 

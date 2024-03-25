2f2416677a2c3aa23de8cb2a1a76faa10b6ba6ac
Ulises <ulisescappa@gmail.com>
2024-03-24 23:05:06 -0300

    Add a logo

--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <a href="http://localhost:5173/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://static.vecteezy.com/system/resources/previews/001/208/283/non_2x/music-guitar-png.png" className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sound Voyage</span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
